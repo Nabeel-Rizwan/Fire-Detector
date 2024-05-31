d59b9d288bb087a431f30e95da5808ce6b68a3ee
Nabeel Mohammad Rizwan <nabeel.rizwan123@ymail.com>
2024-05-31 23:26:25 +0530

    increasing sensitivity to fire

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import cv2
 import math
 
 # Running real time from webcam
-cap = cv2.VideoCapture(0)       #   We can use external camera like CCTV to detect and monitor fire.
+cap = cv2.VideoCapture('video.mp4')       #   We can use external camera like CCTV to detect and monitor fire.
 model = YOLO('fire.pt')         # Trained Model through Yolo
 
 # Reading the classes
@@ -15,17 +15,17 @@ while True:
     frame = cv2.resize(frame,(600,500))     # Frame size
     result = model(frame,stream=True)
 
-    # Getting box,confidence and class names informations to work with. If confidence is more than 70 , It is a fire
+    # Getting box,confidence and class names informations to work with. If confidence is more than 60 , It is a fire
     for info in result:
         boxes = info.boxes
         for box in boxes:
             confidence = box.conf[0]
             confidence = math.ceil(confidence * 100)
             Class = int(box.cls[0])
-            if confidence > 70:
+            if confidence > 60:
                 x1,y1,x2,y2 = box.xyxy[0]
                 x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
-                cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),5)
+                cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,500),5)
                 cvzone.putTextRect(frame, f'{classnames[Class]} {confidence}%', [x1 + 8, y1 + 100], scale=1,thickness=2)    # Text placement in frame
 
 
