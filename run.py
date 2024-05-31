7488c61af9a4a3c10d363ac50c137ecc18760f73
Nabeel Mohammad Rizwan <nabeel.rizwan123@ymail.com>
2024-05-31 17:14:48 +0530

    accuract model trained using yolo

---
+++ run.py
@@ -0,0 +1,36 @@
+from ultralytics import YOLO
+import cvzone
+import cv2
+import math
+
+# Running real time from webcam
+cap = cv2.VideoCapture(0)
+model = YOLO('best.pt')         # Trained Model through Yolo
+
+# Reading the classes
+classnames = ['FIRE']
+
+while True:
+    ret,frame = cap.read()
+    frame = cv2.resize(frame,(600,500))     # Frame size
+    result = model(frame,stream=True)
+
+    # Getting bbox,confidence and class names informations to work with
+    for info in result:
+        boxes = info.boxes
+        for box in boxes:
+            confidence = box.conf[0]
+            confidence = math.ceil(confidence * 100)
+            Class = int(box.cls[0])
+            if confidence > 75:
+                x1,y1,x2,y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
+                cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),5)
+                cvzone.putTextRect(frame, f'{classnames[Class]} {confidence}%', [x1 + 8, y1 + 100],
+                                   scale=1,thickness=2)
+
+
+
+
+    cv2.imshow('frame',frame)
+    cv2.waitKey(1)
