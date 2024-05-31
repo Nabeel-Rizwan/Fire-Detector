408d0557d199298f64a632a354f96add79f2259d
Nabeel Mohammad Rizwan <nabeel.rizwan123@ymail.com>
2024-05-31 17:18:45 +0530

    increased confidence for better detection of fire

--- run.py
+++ run.py
@@ -15,19 +15,18 @@ while True:
     frame = cv2.resize(frame,(600,500))     # Frame size
     result = model(frame,stream=True)
 
-    # Getting bbox,confidence and class names informations to work with
+    # Getting box,confidence and class names informations to work with. If confidence is more than 80, It is a fire
     for info in result:
         boxes = info.boxes
         for box in boxes:
             confidence = box.conf[0]
             confidence = math.ceil(confidence * 100)
             Class = int(box.cls[0])
-            if confidence > 75:
+            if confidence > 80:
                 x1,y1,x2,y2 = box.xyxy[0]
                 x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
                 cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),5)
-                cvzone.putTextRect(frame, f'{classnames[Class]} {confidence}%', [x1 + 8, y1 + 100],
-                                   scale=1,thickness=2)
+                cvzone.putTextRect(frame, f'{classnames[Class]} {confidence}%', [x1 + 8, y1 + 100], scale=1,thickness=2)    # Text placement in frame
 
 
 
